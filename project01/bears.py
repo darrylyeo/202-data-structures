98afe715442530333cc2584e8bd70ab2067c3e36
Darryl Yeo <darrylyeo@gmail.com>
2018-10-03 04:05:12 +0000

    I misinterpreted the instructions! You subtract the bears from your total, not replace it. (The test cases passed for both interpretations.)

--- project01/bears.py
+++ project01/bears.py
@@ -1,22 +1,17 @@
 # Returns whether it is not possible to win the bear game by starting with n bears.
 # (Why bears?)
 
-def bears(n, used = []):
-    if n in used:
-        return False
-    
-    used = [n, *used]
-    
+def bears(n):
     return (
         # The goal of the game is to end up with EXACTLY 42 bears
-        n == 42 or
+        n == 42 if n <= 42 else
         
         # If n is even, then you may give back n/2 bears.
-        (n % 2 == 0 and bears(n / 2, used)) or
+        (n % 2 == 0 and bears(n / 2)) or
             
         # If n is divisible by 3 or 4, then you may multiply the last two digits of n and give back this many bears.
-        ((n % 3 == 0 or n % 4 == 0) and bears((n % 10) * (n // 10 % 10), used)) or
+        ((n % 3 == 0 or n % 4 == 0) and bears(n - (n % 10) * (n // 10 % 10))) or
         
         # If n is divisible by 5, then you may give back 42 bears.
-        (n % 5 == 0 and bears(42, used))
+        (n % 5 == 0 and bears(n - 42))
     )
